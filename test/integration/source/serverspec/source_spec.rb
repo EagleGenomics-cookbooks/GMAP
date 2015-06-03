d900dbdf484caaa0881d7f0171a0bac6360d75a8
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-06-03 11:46:04 +0100

    First working version, specifically for centos-6.5

---
+++ test/integration/source/serverspec/source_spec.rb
@@ -0,0 +1,9 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe command('which gmap') do
+    set :path, '/usr/local/bin:/usr/local/sbin:$PATH'
+    its(:exit_status) { should eq 0 }
+end
