db80a661de227777010af3353884d6f2f7469f21
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-06-04 11:42:50 +0100

    added version test

--- test/integration/source/serverspec/source_spec.rb
+++ test/integration/source/serverspec/source_spec.rb
@@ -7,3 +7,7 @@ set :backend, :exec
 describe command('which gmap') do
   its(:exit_status) { should eq 0 }
 end
+
+describe command('gmap --version') do
+  its(:stdout) { should match(/Part of GMAP package, version 2013-11-27/) }
+end
