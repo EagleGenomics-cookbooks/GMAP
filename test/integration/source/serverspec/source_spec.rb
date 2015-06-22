f63ff5103afde827656ef11663d318734b797dd2
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-06-22 11:24:27 +0100

    Adding tests for executable files

--- test/integration/source/serverspec/source_spec.rb
+++ test/integration/source/serverspec/source_spec.rb
@@ -11,3 +11,21 @@ end
 describe command('gmap --version') do
   its(:stdout) { should match(/Part of GMAP package, version 2013-11-27/) }
 end
+
+['atoiindex', 'cmetindex', 'dbsnp_iit', 'ensembl_genes', 'fa_coords', 'get-genome', 'gff3_genes',
+ 'gff3_introns', 'gff3_splicesites', 'gmap', 'gmap_build', 'gmap_compress', 'gmapindex', 'gmapl',
+ 'gmap_process', 'gmap_reassemble', 'gmap_uncompress', 'gsnap', 'gsnapl', 'gtf_genes',
+ 'gtf_introns', 'gtf_splicesites', 'gvf_iit', 'iit_dump', 'iit_get', 'iit_store', 'md_coords',
+ 'psl_genes', 'psl_introns', 'psl_splicesites', 'snpindex', 'uniqscan', 'uniqscanl',
+ 'vcf_iit'].each do |file_executable|
+  describe command("which #{file_executable}") do
+    its(:exit_status) { should eq 0 }
+  end
+
+  file_path = '/usr/local/bin/' + file_executable
+
+  describe file(file_path) do
+    it { should be_file }
+    it { should be_executable }
+  end
+end
