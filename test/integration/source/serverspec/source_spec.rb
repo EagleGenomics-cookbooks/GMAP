9078646799675e343db2ac0e69d48771fd59eb33
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-06-23 10:29:04 +0100

    Some executables have --version arg, some do not. Array style changed.

--- test/integration/source/serverspec/source_spec.rb
+++ test/integration/source/serverspec/source_spec.rb
@@ -4,20 +4,29 @@ require_relative 'spec_helper'
 # Required by serverspec
 set :backend, :exec
 
-describe command('which gmap') do
-  its(:exit_status) { should eq 0 }
+# Check executables and their --version
+%w(atoiindex cmetindex get-genome gmap gmap_build gmapl gsnap gsnapl iit_dump iit_get iit_store
+   snpindex uniqscan uniqscanl).each do |file_executable|
+  describe command("which #{file_executable}") do
+    its(:exit_status) { should eq 0 }
+  end
+
+  file_path = '/usr/local/bin/' + file_executable
+
+  describe file(file_path) do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe command("#{file_executable} --version") do
+    its(:stdout) { should match(/Part of GMAP package, version 2013-11-27/) }
+  end
 end
 
-describe command('gmap --version') do
-  its(:stdout) { should match(/Part of GMAP package, version 2013-11-27/) }
-end
-
-['atoiindex', 'cmetindex', 'dbsnp_iit', 'ensembl_genes', 'fa_coords', 'get-genome', 'gff3_genes',
- 'gff3_introns', 'gff3_splicesites', 'gmap', 'gmap_build', 'gmap_compress', 'gmapindex', 'gmapl',
- 'gmap_process', 'gmap_reassemble', 'gmap_uncompress', 'gsnap', 'gsnapl', 'gtf_genes',
- 'gtf_introns', 'gtf_splicesites', 'gvf_iit', 'iit_dump', 'iit_get', 'iit_store', 'md_coords',
- 'psl_genes', 'psl_introns', 'psl_splicesites', 'snpindex', 'uniqscan', 'uniqscanl',
- 'vcf_iit'].each do |file_executable|
+# These executables don't take --version arg. Handle differently.
+%w(dbsnp_iit ensembl_genes fa_coords gff3_genes gff3_introns gff3_splicesites gmapindex
+   gmap_compress gmap_process gmap_reassemble gmap_uncompress gtf_genes gtf_introns gtf_splicesites
+   gvf_iit md_coords psl_genes psl_introns psl_splicesites vcf_iit).each do |file_executable|
   describe command("which #{file_executable}") do
     its(:exit_status) { should eq 0 }
   end
