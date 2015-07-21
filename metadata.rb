2ffed6d9267fbb5d2d62a6776a6737b5222c3bf9
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-07-21 15:40:08 +0100

    Preparing for version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@eaglegenomics.com'
 license 'apache2'
 description 'Installs/Configures GMAP'
 long_description 'Installs/Configures GMAP'
-version '0.1.0'
+version '1.0.0'
 
 supports 'centos', '= 6.5'
 
