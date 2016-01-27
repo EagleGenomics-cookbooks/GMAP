0b81b69a6097126a8a4901778ca23a6a37bbd92d
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-27 10:54:59 +0000

    Standardising license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'GMAP'
 maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'apache2'
+license 'Apache v2.0'
 description 'Installs/Configures GMAP'
 long_description 'Installs/Configures GMAP'
 version '1.0.0'
